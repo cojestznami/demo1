95b9365759847ca70c645cebb09f3921466d9492
cojestznami <bartosz@cojestznami.pl>
2022-12-28 21:36:38 +0100

    0.2 next_edits_movingpagescript

--- locators.py
+++ locators.py
@@ -1,3 +1,6 @@
+import time
+
+
 class MoveToResults:
     def __init__(self):
         self.gdpr_decline_xpath = "W0wltc"
@@ -8,3 +11,14 @@ class MoveToResults:
         self.place_confirmation_xpath = "//input[@placeholder='Wpisz kierunek lub hotel']"
         self.results_confirmation_xpath = "//div[1]/div/div[1]/ul/li[2]/label"
         self.search_places_button_xpath = "/html/body/div[5]/div[1]/div/footer/button"
+        self.inside_offer_price_tag = "//div[2]/div/div/div/h4"
+
+
+scrollread = driver.execute_script('return document.body.scrollHeight')
+scrolldown = driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+while True:
+    scrolldown
+    time.sleep(sleeptime)
+    scrollread
+    if scrollread==scrollread
+        break
--- main.py
+++ main.py
@@ -16,5 +16,20 @@ driver.find_element(By.XPATH, "//input[@value='Dowolnie']").click()
 driver.find_element(By.XPATH, "//input[@placeholder='Wpisz kierunek lub hotel']").send_keys("Bieszczady", Keys.ENTER)
 driver.find_element(By.XPATH, "//div[1]/div/div[1]/ul/li[2]/label").click()
 driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/div/footer/button").click()
+
+# driver.find_element(By.XPATH, "//div[1]/section/div[2]/a[1]/div[2]").click()
+# print(driver.find_element(By.XPATH, "//div[2]/div/div/div/h4").text)
+elem = driver.find_element(By.TAG_NAME, "body")
+counter = 0
+while counter < 10:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    counter += 1
+
+list1 = driver.find_elements(By.XPATH, "//div[4]/div[1]/section/div[2]/a")
+list_url = [link.get_attribute("href") for link in list1]
+print(list_url)
+print(list1)
+print(len(list_url))
 time.sleep(90)
 # if __name__ == '__main__':
